e6a42a081f095606999d44c7244edb1aff9edfab
JP <85654561+jp524@users.noreply.github.com>
2022-11-06 17:15:07 -0500

    Build model and controller for User

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, length: { in: 3..20 }
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 6 }
+end
